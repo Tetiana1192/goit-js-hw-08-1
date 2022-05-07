7604a79a3b85463c138c0cc207aa4411725626b1
Tetiana1192 <tanya_strigun@ukr.net>
2022-05-08 01:58:58 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,11 +2,29 @@ import throttle from 'lodash.throttle';
 
 
 const STORAGE_KEY = 'feedback-form-state';
-const formEl = document.querySelector('.feedback-form');
 
-formEl.addEventListener('input', throttle(onTextInput, 500));
-formEl.addEventListener('submit', onFormSubmit);
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+};
 
-getStorageData();
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
 
 
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  
+  evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    
+     formData = {};
