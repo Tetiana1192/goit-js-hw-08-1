da71674d55eb698a37d0c48ba88ef17a5245d143
Tetiana1192 <tanya_strigun@ukr.net>
2022-05-15 19:11:01 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,21 +14,18 @@ textarea.addEventListener('input', evt => {
     persistedFeedBack = persistedFeedBack ? JSON.parse(persistedFeedBack) : {};  //якщо там щось є то парсим його ,якщо нічого нема то фільтри починаються з пустого об`єкта 
     persistedFeedBack[evt.target.name] = evt.target.value;   //зберігаємо або добавляємо значення 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFeedBack));
-  });
+  
+
+});
 
 
 function onTextareaInput(event) {
   persistedFeedBack[event.target.name] = event.target.value;
-
   localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFeedBack));
+  
 }
 
-
-
-
 initForm(); //при ініцілізації форми витянули обєкт 
-
-
 feedBackForm.addEventListener('submit', evt => {
   evt.preventDefault();
   // console.log(feedBackForm.elements);
@@ -36,39 +33,37 @@ feedBackForm.addEventListener('submit', evt => {
   formData.forEach((value, name) => console.log(value, name));
 });
 
-// робимо делегування на форму 
-  // feedBackForm.addEventListener('change', evt => {    //вішаємо нового слухача,один обработчик вішає на всіх інпута слухача  
-  // console.log(evt.target.name);   // підставляємо ім`я ключа
-  // console.log(evt.target.value);  // ставомо значення ключа
-  
-  // selectedFeedBack[evt.target.name] = evt.target.value;
-  // console.log(selectedFeedBack);
-  // потрібно зберігати зміни в локальному сховащі ,потрібно зробити об`єкт, 
-  // localStorage.setItem('selectedFeedBack', JSON.stringify(selectedFeedBack));
-//   let persistedFeedBack = localStorage.getItem(STORAGE_KEY);    // беремо з локасторіджа що збережено 
-//     persistedFeedBack = persistedFeedBack ? JSON.parse(persistedFeedBack) : {};  //якщо там щось є то парсим його ,якщо нічого нема то фільтри починаються з пустого об`єкта 
-//     persistedFeedBack[evt.target.name] = evt.target.value;   //зберігаємо або добавляємо значення 
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFeedBack));
-    
-// });
 
 
 //щоб зберігалися значення при перезагрузці потрібно зробити функцію 
 function initForm() {
-  let persistedFeedBack = localStorage.getItem(STORAGE_KEY);  
-    // щоб розпарсити потрібно зробити перевірку ,якщо фільтри є то зними будемо працювати ,якщо нема то не будем  
+  let persistedFeedBack = localStorage.getItem(STORAGE_KEY);
+  // щоб розпарсити потрібно зробити перевірку ,якщо фільтри є то зними будемо працювати ,якщо нема то не будем  
   if (persistedFeedBack) {
     persistedFeedBack = JSON.parse(persistedFeedBack);  //конструкція трай 
     // console.log(persistedFeedBack);
     Object.entries(persistedFeedBack).forEach(([name, value]) => {    //взяли всі його входження
-      // selectedFeedBack[name] = value;    
-      feedBackForm.elements[name].value = value.email; 
-      onTextareaInput.elements[name].value = value.message;
+      
+      // feedBackForm.elements[name].value = value.email;
       
 
+      if (name === 'message') {
+        feedBackForm.elements[name].value = value;
+        console.log('message');
+      }
+
+       if (name === 'email') {
+         onTextareaInput.elements[name].value = value;
+         console.log('email');
+      }
+     
+      
+    
     });
-}
-}
+  }
+};
+// }якщо name === ‘message’ тоді feedBackForm.elements[name].value = value;
+// якщо name === ‘name’ тоді feedBackForm.elements[name].value = value;
 
 feedBackForm.addEventListener('reset', () => {
   localStorage.removeItem(STORAGE_KEY);
