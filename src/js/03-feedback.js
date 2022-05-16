1c7cb09a6cb495a4e695bc1d94fc43cbeeacdccf
Tetiana1192 <tanya_strigun@ukr.net>
2022-05-16 19:44:31 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,70 +1,29 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
-const feedBackForm = document.querySelector('.feedback-form');   //дивимося чи працює форма
-const textarea = document.querySelector('.feedback-form  textarea');
+const form = document.querySelector('.feedback-form');
+const mail = form.querySelector('input');
+const message = form.querySelector('textarea');
 
-//  const selectedFeedBack = {};     //створюємо обьюкт 
-
-
-
-textarea.addEventListener('input', evt => {
-  throttle(onTextareaInput, 200);
-  let persistedFeedBack = localStorage.getItem(STORAGE_KEY);    // беремо з локасторіджа що збережено 
-    persistedFeedBack = persistedFeedBack ? JSON.parse(persistedFeedBack) : {};  //якщо там щось є то парсим його ,якщо нічого нема то фільтри починаються з пустого об`єкта 
-    persistedFeedBack[evt.target.name] = evt.target.value;   //зберігаємо або добавляємо значення 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFeedBack));
-  
-
-});
-
-
-function onTextareaInput(event) {
-  persistedFeedBack[event.target.name] = event.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFeedBack));
-  
+const formValue = ()=>{
+    const values = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (!values) {return;}
+mail.value = values.mail;
+message.value= values.message;
 }
 
-initForm(); //при ініцілізації форми витянули обєкт 
-feedBackForm.addEventListener('submit', evt => {
-  evt.preventDefault();
-  // console.log(feedBackForm.elements);
-  const formData = new FormData(feedBackForm);     // збирає всі значення полів в одне ціле 
-  formData.forEach((value, name) => console.log(value, name));
-});
 
+const saveFormValue = (e) => {
+    localStorage.setItem("feedback-form-state", `{"mail":"${mail.value}","message":"${message.value}"}`);
+}
 
+const formSubmit = e =>{
+    e.preventDefault();
+    console.log({mail: mail.value,message: message.value});
+    mail.value='';
+    message.value='';
+    localStorage.removeItem('feedback-form-state');
 
-//щоб зберігалися значення при перезагрузці потрібно зробити функцію 
-function initForm() {
-  let persistedFeedBack = localStorage.getItem(STORAGE_KEY);
-  // щоб розпарсити потрібно зробити перевірку ,якщо фільтри є то зними будемо працювати ,якщо нема то не будем  
-  if (persistedFeedBack) {
-    persistedFeedBack = JSON.parse(persistedFeedBack);  //конструкція трай 
-    // console.log(persistedFeedBack);
-    Object.entries(persistedFeedBack).forEach(([name, value]) => {    //взяли всі його входження
-      
-      // feedBackForm.elements[name].value = value.email;
-      
-
-      if (name === 'message') {
-        feedBackForm.elements[name].value = value;
-        console.log('message');
-      }
-
-       if (name === 'email') {
-         onTextareaInput.elements[name].value = value;
-         console.log('email');
-      }
-     
-      
-    
-    });
-  }
-};
-// }якщо name === ‘message’ тоді feedBackForm.elements[name].value = value;
-// якщо name === ‘name’ тоді feedBackForm.elements[name].value = value;
-
-feedBackForm.addEventListener('reset', () => {
-  localStorage.removeItem(STORAGE_KEY);
-});
+}
+form.addEventListener("input", throttle (saveFormValue ,  500 ));
+form.addEventListener('submit',formSubmit);
+formValue();
